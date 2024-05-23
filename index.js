b336b15eb06793880c510cda5c89616498fbb923
Andre Manzo <124328360+andremanzo1@users.noreply.github.com>
2024-05-23 04:09:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,373 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const pool = dbConnection();
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+
+app.set("trust proxy", 1); // trust first proxy
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  }),
+);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+//to parse Form data sent using POST method
+app.use(express.urlencoded({ extended: true }));
+
+//routes
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+});
+
+//Userhome
+app.get("/UserHome", (req, res) => {
+  let username = req.session.username;
+  res.render("Userhome", { username: username });
+});
+
+//Adminhome
+app.get("/Adminhome", (req, res) => {
+  res.render("AdminHome");
+});
+
+//Display registered Users from the admin side
+app.get("/DisplayUsers", async (req, res) => {
+  let sql = `SELECT * FROM Customers`;
+  let rows = await executeSQL(sql);
+  res.render("DisplayUsers", { users: rows });
+});
+
+// Option to delete a user
+app.get("/DeleteUser", async (req, res) => {
+  let deleteId = req.query.CustomerID;
+  let sql = `DELETE FROM Customers WHERE CustomerID = ?`;
+
+  let params = [deleteId];
+  let rows = await executeSQL(sql, params);
+  res.redirect("/DisplayUsers");
+});
+
+//option to add products to the store
+app.get("/addProduct", (req, res) => {
+  res.render("newProduct");
+});
+
+app.post("/addProduct", async (req, res) => {
+  let Name = req.body.Name;
+  let Description = req.body.Description;
+  let Price = req.body.Price;
+  let QuantityAvailable = req.body.QuantityAvailable;
+  let Category = req.body.Category;
+  let Supplier = req.body.Supplier;
+  let Image = req.body.Image;
+
+  let query = `INSERT INTO Products (Name, Description, Price, QuantityAvailable, Category, Supplier, Image ) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+  let params = [
+    Name,
+    Description,
+    Price,
+    QuantityAvailable,
+    Category,
+    Supplier,
+    Image,
+  ];
+  let result = await executeSQL(query, params);
+  console.log(result);
+  res.redirect("/addProduct");
+});
+
+//Display list products from the admin side
+app.get("/productList", async (req, res) => {
+  let sql = `SELECT * FROM Products ORDER BY Name`;
+
+  let rows = await executeSQL(sql);
+  res.render("productList", { productList: rows });
+});
+
+//Display form to update product
+app.get("/updateProduct", async (req, res) => {
+  let id = req.query.ProductID;
+
+  let sql = `SELECT *
+              FROM Products
+              WHERE ProductID = ?`;
+  let rows = await executeSQL(sql, [id]);
+  res.render("updateProduct", { productInfo: rows[0] });
+});
+//Updates product info
+app.post("/updateProduct", async (req, res) => {
+  let id = req.body.ProductID;
+  let Name = req.body.Name;
+  let Description = req.body.Description;
+  let Price = req.body.Price;
+  let QuantityAvailable = req.body.QuantityAvailable;
+  let Category = req.body.Category;
+  let Supplier = req.body.Supplier;
+  let Image = req.body.Image;
+  let sql = `UPDATE Products
+              SET Name = ?,
+              Description = ?,
+              Price = ?,
+              QuantityAvailable = ?,
+              Category=?,
+              Supplier=?,
+              Image=?
+              WHERE ProductID = ?`;
+  let params = [
+    Name,
+    Description,
+    Price,
+    QuantityAvailable,
+    Category,
+    Supplier,
+    Image,
+    id,
+  ];
+  let rows = await executeSQL(sql, params);
+
+  res.redirect("/productList");
+});
+
+// Option to delete a product
+app.get("/DeleteProduct", async (req, res) => {
+  let deleteId = req.query.ProductID;
+  let sql = `DELETE FROM Products WHERE ProductID = ?`;
+
+  let params = [deleteId];
+  let rows = await executeSQL(sql, params);
+  res.redirect("/productList");
+});
+
+//Login
+app.get("/Login", (req, res) => {
+  res.render("LoginUser");
+});
+
+app.post("/Login", async (req, res) => {
+  let UserName = req.body.UserName;
+  let Password = req.body.Password;
+
+  if (!UserName || !Password) {
+    // If any required field is missing, redirect back to create account page
+    return res.render("LoginUser");
+  }
+
+  try {
+    let query = `SELECT CustomerID, UserName, IsAdmin , Password
+                FROM Customers 
+                WHERE UserName = ?`;
+
+    let params = [UserName];
+
+    let result = await executeSQL(query, params);
+    if (result.length > 0) {
+      const hashedPassword = result[0].Password;
+
+      // Compare the hashed password with the password provided by the user
+      const passwordMatch = await bcrypt.compare(Password, hashedPassword);
+
+      if (passwordMatch) {
+        req.session.customerID = result[0].CustomerID;
+        req.session.username = result[0].UserName;
+        // If passwords match, the user is authenticated
+        if (result[0].IsAdmin === 1) {
+          // If user is admin, render the AdminHome page
+          return res.redirect("/Adminhome");
+        } else {
+          // If user is not an admin, redirect to UserHome page with the username parameter
+          return res.redirect(
+            "/UserHome?username=" + encodeURIComponent(UserName),
+          );
+        }
+      } else {
+        // If passwords don't match, render the login page
+        return res.render("LoginUser");
+      }
+    } else {
+      // If no matching user is found, render the login page
+      return res.render("LoginUser");
+    }
+  } catch (error) {
+    return res.render("LoginUser");
+  }
+});
+
+// Create Account
+app.get("/CreateAccount", async (req, res) => {
+  res.render("newUser");
+});
+
+app.post("/CreateAccount", async (req, res) => {
+  let UserName = req.body.UserName;
+  let FirstName = req.body.FirstName;
+  let LastName = req.body.LastName;
+  let Email = req.body.Email;
+  let Password = req.body.Password;
+  let Address = req.body.Address;
+  let City = req.body.City;
+  let State = req.body.State;
+  let ZipCode = req.body.ZipCode;
+  let Country = req.body.Country;
+  let Phone = req.body.Phone;
+
+  if (
+    !UserName ||
+    !FirstName ||
+    !LastName ||
+    !Email ||
+    !Password ||
+    !Address ||
+    !City ||
+    !State ||
+    !ZipCode ||
+    !Country ||
+    !Phone
+  ) {
+    // If any required field is missing, redirect back to create account page
+    return res.render("newUser");
+  }
+
+  try {
+    //To check if the username has been taken or not
+    let check = `SELECT COUNT(*) AS count FROM Customers WHERE UserName = ?`;
+    let checkParam = [UserName];
+    let checkResult = await executeSQL(check, checkParam);
+    if (checkResult[0].count > 0) {
+      // If the username already exists, it will redirect back to create account page
+      return res.render("newUser");
+    }
+
+    const hashedPassword = await bcrypt.hash(Password, 10);
+    let query = `INSERT INTO Customers (UserName, FirstName, LastName, Email, Password, Address, City, State, ZipCode, Country, Phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+    let params = [
+      UserName,
+      FirstName,
+      LastName,
+      Email,
+      hashedPassword,
+      Address,
+      City,
+      State,
+      ZipCode,
+      Country,
+      Phone,
+    ];
+
+    let result = await executeSQL(query, params);
+    // If the account is successfully created, render the Userhome view
+    req.session.customerID = result.insertId;
+    req.session.username = UserName;
+    res.redirect("/UserHome?username=" + encodeURIComponent(UserName));
+  } catch (error) {
+    if (error.code === "ER_DUP_ENTRY") {
+      return res.redirect("newUser");
+    }
+    res.render("newUser");
+  }
+});
+
+app.get("/viewProducts", async (req, res) => {
+  let sql = `SELECT * FROM Products WHERE QuantityAvailable > 0 ORDER BY Name`;
+  let rows = await executeSQL(sql);
+  res.render("viewProducts", { productList: rows });
+});
+
+app.get("/viewCart", async (req, res) => {
+  let customerID = req.session.customerID;
+  let sql = `SELECT Products.Name, Products.Description, Products.Price, ShoppingCart.Quantity, Products.ProductID
+             FROM ShoppingCart
+             JOIN Products ON ShoppingCart.ProductID = Products.ProductID
+             WHERE ShoppingCart.CustomerID = ?`;
+  let rows = await executeSQL(sql, [customerID]);
+  res.render("viewCart", { cartItems: rows });
+});
+
+app.post("/addToCart", async (req, res) => {
+  let customerID = req.session.customerID;
+  let productID = req.body.ProductID;
+  let quantity = parseInt(req.body.Quantity);
+  let productSql = `SELECT QuantityAvailable
+                    FROM Products 
+                    WHERE ProductID = ?`;
+  let product = await executeSQL(productSql, [productID]);
+
+  if (product.length > 0 && product[0].QuantityAvailable >= quantity) {
+    let newQuantity = product[0].QuantityAvailable - quantity;
+    let updateProductSql = `UPDATE Products
+                            SET QuantityAvailable = ?
+                            WHERE ProductID = ?`;
+    await executeSQL(updateProductSql, [newQuantity, productID]);
+    let checkCartSql = `SELECT *
+                        FROM ShoppingCart
+                        WHERE CustomerID = ?
+                        AND ProductID = ?`;
+    let cartItem = await executeSQL(checkCartSql, [customerID, productID]);
+
+    if (cartItem.length > 0) {
+      let newCartQuantity = parseInt(cartItem[0].Quantity) + quantity;
+      let updateCartSql = `UPDATE ShoppingCart
+                         SET Quantity = ?
+                         WHERE CustomerID = ?
+                         AND ProductID = ?`;
+      await executeSQL(updateCartSql, [newCartQuantity, customerID, productID]);
+    } else {
+      let addCartSql = `INSERT INTO ShoppingCart (CustomerID, ProductID, Quantity) VALUES (?, ?, ?)`;
+      await executeSQL(addCartSql, [customerID, productID, quantity]);
+    }
+    res.redirect("/viewProducts");
+  }
+});
+
+app.post("/checkout", async (req, res) => {
+  let customerID = req.session.customerID;
+  let sql = `DELETE 
+             FROM ShoppingCart 
+             WHERE CustomerID = ?`;
+  await executeSQL(sql, [customerID]);
+  res.render("orderConfirm");
+});
+
+app.get("/dbTest", async function (req, res) {
+  let sql = "SELECT CURDATE()";
+  let rows = await executeSQL(sql);
+  res.send(rows);
+}); //dbTest
+
+//functions
+async function executeSQL(sql, params) {
+  return new Promise(function (resolve, reject) {
+    pool.query(sql, params, function (err, rows, fields) {
+      if (err) throw err;
+      resolve(rows);
+    });
+  });
+} //executeSQL
+//values in red must be updated
+function dbConnection() {
+  const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: "wm63be5w8m7gs25a.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "ct66i9sfgn5eot0d",
+    password: "ogicf62m9waw6gga",
+    database: "e94hmvgxib7qgtfn",
+  });
+
+  return pool;
+} //dbConnection
+
+//start server
+app.listen(3000, () => {
+  console.log("Expresss server running...");
+});
---
+++ public/js/createAccount.js
@@ -0,0 +1,181 @@
+document.querySelector("form").addEventListener("submit", validation)
+document.querySelector("#state").addEventListener("click", populateStates)
+
+async function displayRandomBackground() {
+  let url = `https://api.unsplash.com/photos/random/?client_id=UmG_IJB6rHWGjQYr8-DsaDQlUDAUQImkoxT218vZ5mY&featured=true&query=rainforest&orientation=landscape`
+  let response = await fetch(url)
+  let data = await response.json()
+  let body = document.querySelector("body")
+  body.style.backgroundImage = `url(${data.urls.full})`;
+  body.style.color = "white";
+}
+displayRandomBackground();
+
+async function populateStates() {
+  let url = "https://csumb.space/api/allStatesAPI.php"
+  let statesResponse = await fetch(url);
+  let statesData = await statesResponse.json();
+
+  for (let s of statesData) {
+    let option = document.createElement("option");
+    option.id = s.id;
+    option.value = s.usps;
+    console.log(s.usps);
+    option.textContent = s.usps;
+    document.querySelector("#state").appendChild(option);
+  }
+}
+populateStates();
+
+function validation(event) {
+    let error = false;
+    let user = document.querySelector("input[name=UserName]").value
+    let fname = document.querySelector("input[name=FirstName]").value
+    let lname = document.querySelector("input[name=LastName]").value
+    let email = document.querySelector("input[name=Email]").value
+    let pass = document.querySelector("input[name=Password]").value
+    let address = document.querySelector("input[name=Address]").value
+    let city = document.querySelector("input[name=City]").value
+    let state = document.querySelector("#state").value
+    let zip = document.querySelector("input[name=ZipCode]").value
+    let country = document.querySelector("input[name=Country]").value
+    let phone = document.querySelector("input[name=Phone]").value
+    error = false;
+
+    if (!user) {
+      document.querySelector("#error").innerHTML = "Please enter a username"
+      document.querySelector("#error").style.color = "red"
+      error = true;
+    } else {
+      if (user.length > 50) {
+        document.querySelector("#error").innerHTML = "Your username is too long"
+        document.querySelector("#error").style.color = "red"
+        error = true;
+      }
+    }
+
+    if(!fname) {
+      document.querySelector("#error2").innerHTML = "Please enter your first name"
+      document.querySelector("#error2").style.color = "red"
+      error = true;
+    } else {
+      if (fname.length > 50) {
+        document.querySelector("#error2").innerHTML = "Your first name is too long"
+        document.querySelector("#error2").style.color = "red"
+        error = true;
+      }
+    }
+
+    if (!lname) {
+      document.querySelector("#error3").innerHTML = "Please enter your last name"
+      document.querySelector("#error3").style.color = "red"
+      error = true;
+    } else {
+      if (lname.length > 50) {
+        document.querySelector("#error3").innerHTML = "Your last name is too long"
+        document.querySelector("#error3").style.color = "red"
+        error = true;
+      }
+    }
+
+    if (!email) {
+      document.querySelector("#error4").innerHTML = "Please enter your email"
+      document.querySelector("#error4").style.color = "red"
+      error = true;
+    } else {
+      if (email.length > 100) {
+        document.querySelector("#error4").innerHTML = "Your email is too long"
+        document.querySelector("#error4").style.color = "red"
+        error = true;
+      }
+    }
+
+    if (!pass) {
+      document.querySelector("#error5").innerHTML = "Please enter a password"
+      document.querySelector("#error5").style.color = "red"
+      error = true;
+    } else {
+      if (pass.length > 100) {
+        document.querySelector("#error5").innerHTML = "Your password is too long"
+        document.querySelector("#error5").style.color = "red"
+        error = true;
+      }
+    }
+
+    if (!address) {
+      document.querySelector("#error6").innerHTML = "Please enter your address"
+      document.querySelector("#error6").style.color = "red"
+      error = true;
+    } else {
+      if (address.length > 255) {
+        document.querySelector("#error6").innerHTML = "Your address is too long"
+        document.querySelector("#error6").style.color = "red"
+        error = true;
+      }
+    }
+
+    if (!city) {
+      document.querySelector("#error7").innerHTML = "Please enter your city"
+      document.querySelector("#error7").style.color = "red"
+      error = true;
+    } else {
+      if (city.length > 100) {
+        document.querySelector("#error7").innerHTML = "Your city is too long"
+        document.querySelector("#error7").style.color = "red"
+        error = true;
+      }
+    }
+
+    if (!state) {
+      document.querySelector("#error8").innerHTML = "Please enter your state"
+      document.querySelector("#error8").style.color = "red"
+      error = true;
+    } else {
+      if (state.length > 100) {
+        document.querySelector("#error8").innerHTML = "Your state is too long"
+        document.querySelector("#error8").style.color = "red"
+        error = true;
+      }
+    }
+
+    if (!zip) {
+      document.querySelector("#error9").innerHTML = "Please enter your zipcode"
+      document.querySelector("#error9").style.color = "red"
+      error = true;
+    } else {
+      if (zip.length > 20) {
+        document.querySelector("#error9").innerHTML = "Your zip is too long"
+        document.querySelector("#error9").style.color = "red"
+        error = true;
+      }
+    }
+
+    if (!country) {
+      document.querySelector("#error10").innerHTML = "Please enter your country"
+      document.querySelector("#error10").style.color = "red"
+      error = true;
+    } else {
+      if (country.length > 100) {
+        document.querySelector("#error10").innerHTML = "Your country is too long"
+        document.querySelector("#error10").style.color = "red"
+        error = true;
+      }
+    }
+
+    if (!phone) {
+      document.querySelector("#error11").innerHTML = "Please enter your country"
+      document.querySelector("#error11").style.color = "red"
+      error = true;
+    } else {
+      if (phone.length > 100) {
+        document.querySelector("#error11").innerHTML = "Your country is too long"
+        document.querySelector("#error11").style.color = "red"
+        error = true;
+      }
+    }
+
+    if (error) {
+      event.preventDefault()
+    }
+    error = false;
+}
